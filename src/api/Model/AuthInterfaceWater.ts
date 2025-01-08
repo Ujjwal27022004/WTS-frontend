3bb28003e24d461dfba457e545bc5598eb7a422b
Vedant Shrirao <122175901+VedantShrirao18@users.noreply.github.com>
2025-01-08 12:44:35 +0530

    Commit

--- src/api/Model/AuthInterfaceWater.ts
+++ src/api/Model/AuthInterfaceWater.ts
@@ -1,7 +1,13 @@
 export interface LoginBasicInfo {
     email: string;
     password: string;
-  }
-  
+}
+
+export interface SignUpBasicInfo {
+    username : string;
+    email: string;
+    password: string;
+}
+
   
   
--- src/api/Service/AuthService.tsx
+++ src/api/Service/AuthService.tsx
@@ -1,19 +1,29 @@
 import axios from "axios";
-import { LoginBasicInfo } from "../Model/AuthInterfaceWater";
+import { LoginBasicInfo , SignUpBasicInfo } from "../Model/AuthInterfaceWater";
 
 const API_URL = import.meta.env.VITE_APP_API_URL;
 
 export const AuthService = {
-  login: async (loginData: LoginBasicInfo): Promise<LoginBasicInfo> => {
+  login: async (loginData: LoginBasicInfo): Promise<any> => {
     try {
-      const response = await axios.post<LoginBasicInfo>(
-        `${API_URL}/api/v1/user/login`,
-        loginData
+      const response = await axios.post(`${API_URL}/api/v1/user/login`, loginData);
+      return response.data; // Return the API response data
+    } catch (error: any) {
+      console.error("Error during login:", error.response?.data || error.message);
+      throw new Error(
+          error.response?.data?.message || "Login failed. Please try again."
       );
-      return response.data; // Return the entire login data (email, password, etc.)=
-    } catch (error) {
-      console.error("Error during login:", error);
-      throw error; // Rethrow the error to be handled in the component
+    }
+  },
+  signup: async (SignUpData: SignUpBasicInfo): Promise<any> => {
+    try {
+      const response = await axios.post(`${API_URL}/api/v1/user/signup`,SignUpData);
+      return response.data; // Return the API response data
+    } catch (error: any) {
+      console.error("Error during signup:", error.response?.data || error.message);
+      throw new Error(
+          error.response?.data?.message || "Signup failed. Please try again."
+      );
     }
   },
 };
